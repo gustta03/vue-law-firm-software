6e3aa65f59db92c3aeb3558c0ad6aac4f1c39d66
Gustavo Alves <gustavo_alves777@outlook.com>
2023-09-26 19:18:31 -0300

    making API resquest to save user

--- src/app/views/LoginView.vue
+++ src/app/views/LoginView.vue
@@ -1,39 +1,96 @@
 <script setup lang="ts">
-import { makeRemoteAddAccount } from '../../main/factories/usecases/load-customer-factory'
-import { onMounted, ref } from 'vue'
+import { makeCacheStorage } from '../../main/factories/cache/local-storage-factory';
+import { makeRemoteSaveUser } from '../../main/factories/usecases/save-user-factory'
+import { ref } from 'vue'
+import router from '../router/index'
 
-// Define the type for CustomerResultModel to match the expected structure
-type CustomerResultModel = {
-  name: string
-  phone: string
-  email: string
-  address: string
-  cpfCnpj: string
-  dateOfBirth: string
-  gender: string
-  maritalStatus: string
-  profession: string
-  nationality: string
-  observations: string
-  documents: any[]
+const addAccount = makeRemoteSaveUser()
+const cache = makeCacheStorage()
+
+const data = ref('')
+const name = ref('')
+const email = ref('')
+const password = ref('')
+
+
+function setValueOnSubmitUser(response: string) {
+  data.value = response
+  cache.set('admin:accessToken', response)
+  router.push('/')
 }
 
-const addAccount = makeRemoteAddAccount()
-
-const data = ref<CustomerResultModel[]>([])
-
-onMounted(() => {
-  addAccount.load().then((res) => {
-    data.value = res as unknown as CustomerResultModel[]
+async function handleSubmitUser() {
+  const addAccountRequest = await addAccount.save({
+    email: email.value,
+    name: name.value,
+    password: password.value
   })
-})
+  if (addAccountRequest.accessToken) {
+    setValueOnSubmitUser(addAccountRequest.accessToken)
+  }
+}
+
+
 </script>
 
 <template>
-  <main>
-    <div v-for="prop in data" :key="prop.gender">
-      {{ prop.name }}
-    </div>
-  </main>
+  <div class="login-form-container">
+    <h1>Acesse a plataforma</h1>
+    <p>Faça login ou registre-se para começar a gerenciar seu escritório ainda hoje.</p>
+    <form @submit.prevent="handleSubmitUser" class="form-container">
+      <div>
+        <label for="">Nome</label>
+        <input placeholder="Digite seu email" required v-model="name" />
+      </div>
+      <div>
+        <label for="">Email</label>
+        <input placeholder="Digite seu email" required v-model="email" />
+      </div>
+      <div>
+        <label>Senha</label>
+        <input type="password" placeholder="Digite sua senha" required v-model="password" />
+      </div>
+
+      <button @submit.prevent="handleSubmitUser">Entrar</button>
+
+    </form>
+  </div>
 </template>
-../app/components/TheWelcome.vue
+
+<style>
+.login-form-container {
+  width: 60%;
+  margin: 50px auto;
+  height: 80vh;
+}
+
+.form-container {
+  text-align: start;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0 auto;
+  width: 80%;
+  height: 20rem;
+}
+
+input {
+  display: block;
+  width: 90%;
+  margin: 0 auto;
+  border: 1px solid #CBD5E1;
+  margin: 0 0 20px 0;
+  cursor: pointer;
+  padding: 10px 20px 10px 5px;
+}
+
+button {
+  width: 94%;
+  height: 54px;
+  cursor: pointer;
+  border: 0;
+  border-radius: 4px;
+  background-color: dodgerblue;
+  color: #FFFF;
+}
+</style>
