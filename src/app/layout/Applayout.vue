448c4f91e366512d35335ec70a7ba8e29ec17039
Gustavo Alves <gustavo_alves777@outlook.com>
2023-09-26 19:55:22 -0300

    add layout layer

---
+++ src/app/layout/Applayout.vue
@@ -0,0 +1,13 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+const aap = 1
+</script>
+
+<template>
+  <div class="login-form-container">
+
+  </div>
+</template>
+
+<style>
+</style>
