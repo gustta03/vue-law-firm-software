461c72b3afa92058b84c27bfd61f3f3f563443c9
Gustavo Alves <gustavo_alves777@outlook.com>
2023-09-26 14:59:14 -0300

    moving factories to its own folder

--- src/infra/repository/http-load-customers.ts
+++ src/infra/repository/http-load-customers.ts
@@ -3,10 +3,13 @@ import { HttpClient } from '../contracts/http-protocol'
 import { Repository } from '../contracts/http-repository'
 
 export class HttpLoadCustomer implements Repository {
-  constructor(private readonly httpClient: HttpClient<CustomerResultModel>) {}
+  constructor(
+    private apiUrl: string,
+    private readonly httpClient: HttpClient<CustomerResultModel>
+  ) {}
   async load() {
     const HttpResponse = await this.httpClient.request({
-      url: 'http://localhost:5858/api/customer/all',
+      url: `${this.apiUrl}`,
       method: 'get'
     })
     return HttpResponse
--- src/main/factories/usecases/load-customer-factory.ts
+++ src/main/factories/usecases/load-customer-factory.ts
@@ -1,10 +1,11 @@
 import { AxiosHttpClient } from '../../../infra/http/http-client'
 import { HttpLoadCustomer } from '../../../infra/repository/http-load-customers'
 import { LoadAllCustomerUseCase } from '../../../usecases/http-load-customers'
+import { makeApiUrl } from '../http/api-url-factory'
 
 export const makeRemoteAddAccount = () => {
   const httpClient = new AxiosHttpClient()
-  const httpRepository = new HttpLoadCustomer(httpClient)
+  const httpRepository = new HttpLoadCustomer(makeApiUrl('customer/all'), httpClient)
   const customerUseCase = new LoadAllCustomerUseCase(httpRepository)
   return customerUseCase
 }
