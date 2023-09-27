928e2e7e90ae5055d4d25b3ac477f5fe6d6ecfbd
Gustavo Alves <gustavo_alves777@outlook.com>
2023-09-27 16:07:22 -0300

    add component mainContent to re-use on aplication

---
+++ src/app/components/MainContent.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const contentType = defineProps({
+  pageTitle: String
+})
+</script>
+
+<template>
+  <div class="main-workspace-content">
+    <div class="header-buttons">
+      <h2>{{ contentType.pageTitle }}</h2>
+      <button>Adicionar tarefa</button>
+    </div>
+
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+.main-workspace-content {
+  margin: 0 auto;
+  border-radius: 5px;
+  background-color: rgb(255, 255, 255);
+  height: 90vh;
+  margin-top: 20px;
+  width: 95%;
+}
+
+h2 {
+  width: 15rem;
+}
+
+.header-buttons {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+}
+
+.header-buttons > button {
+  margin-right: 20px;
+  height: 40px;
+  border-radius: 5px;
+  background-color: #228BE6;
+  width: 10rem;
+}
+</style>
