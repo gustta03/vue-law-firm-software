7475af883cbda8113936c2f100d532d0d3ff34d8
Gustavo Alves <gustavo_alves777@outlook.com>
2023-10-06 19:25:14 -0300

    refactor util userIsLogged name

--- src/app/utils/tokenValidator.ts
+++
@@ -1,8 +0,0 @@
-import { makeCacheStorage } from '../../main/factories/cache/local-storage-factory'
-
-const cache = makeCacheStorage()
-
-export function isAuthenticated(): boolean {
-  const hasToken = cache.get('admin:accessToken')
-  return !!hasToken
-}
