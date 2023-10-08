c90acf67c739be12dd0e7ff9a846fd7e837f87f8
Gustavo Alves <gustavo_alves777@outlook.com>
2023-10-08 13:18:48 -0300

    add popup to show customer datails

--- src/app/views/Customers/CustomersView.vue
+++ src/app/views/Customers/CustomersView.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
 import MainContent from '../../components/MainContent.vue'
 
-import { DxDataGrid, DxEditing, DxPaging, DxColumn, DxLookup } from 'devextreme-vue/data-grid'
-import { DxPopup } from 'devextreme-vue/popup'
+import { DxDataGrid, DxColumn, DxPaging, DxEditing, DxForm, DxItem } from 'devextreme-vue/data-grid'
+
 import DataSource from 'devextreme/data/data_source'
-import { DxButton } from 'devextreme-vue'
+import { DxPopup, DxButton } from 'devextreme-vue'
 import { DxToast } from 'devextreme-vue/toast'
 
 import { ref } from 'vue'
 import { dependencies } from '../../dep'
-
 
 const { deleteCustomer, editCustomer, saveCustomer, loadCustomer } = dependencies
 const isToastVisible = ref(false)
@@ -30,6 +29,8 @@ const formData = ref({
   observations: ''
 })
 
+const isModalRowOpen = ref(false)
+const selectedDocument = ref()
 const isModalOpen = ref(false)
 
 const customerData = new DataSource({
@@ -65,8 +66,8 @@ async function handleSaveCustomerOnClick() {
 }
 
 const reloadDataGridWithNewRow = () => {
-  customerData.reload();
-};
+  customerData.reload()
+}
 
 function showToast({ message, type }: { message: string; type: string }) {
   isToastVisible.value = true
@@ -74,6 +75,10 @@ function showToast({ message, type }: { message: string; type: string }) {
   toastMessage.value = message
 }
 
+const handleRowClick = (e) => {
+  selectedDocument.value = e.data
+  isModalRowOpen.value = true
+}
 </script>
 
 <template>
@@ -85,20 +90,45 @@ function showToast({ message, type }: { message: string; type: string }) {
       <DxDataGrid
         :data-source="customerData"
         :allow-adding="true"
+        @row-click="handleRowClick"
         :show-borders="true"
         :focused-row-enabled="true"
         :allow-updating="true"
         :allow-deleting="true"
         :select-text-on-edit-start="true"
         :use-icons="true"
-        key-expr="customerId"
+        key-expr="_id"
       >
         <DxColumn data-field="name" caption="Nome" />
         <DxColumn data-field="address" caption="Endereço" />
         <DxColumn data-field="email" caption="Email" />
         <DxColumn data-field="phone" caption="Telefone" />
+        <DxColumn data-field="cpfCnpj" caption="CPF/CNPJ" />
+        <DxColumn data-field="dateOfBirth" caption="Data de Nascimento" />
+        <DxColumn data-field="gender" caption="Gênero" />
+        <DxColumn data-field="maritalStatus" caption="Estado Civil" />
+        <DxColumn data-field="profession" caption="Profissão" />
+        <DxColumn data-field="nationality" caption="Nacionalidade" />
+        <DxColumn data-field="observations" caption="Observações" />
         <DxEditing :allow-updating="true" :allow-deleting="true" />
         <DxLookup :data-source="customerData" value-expr="_id" display-expr="Ações" />
+
+        <DxEditing :allow-updating="true" :allow-deleting="true" mode="popup">
+          <DxForm>
+            <DxItem data-field="name" caption="Nome" />
+            <DxItem data-field="phone" caption="Telefone" />
+            <DxItem data-field="email" caption="Email" />
+            <DxItem data-field="address" caption="Endereço" />
+            <DxItem data-field="cpfCnpj" caption="CPF/CNPJ" />
+            <DxItem data-field="dateOfBirth" caption="Data de Nascimento" />
+            <DxItem data-field="gender" caption="Gênero" />
+            <DxItem data-field="maritalStatus" caption="Estado Civil" />
+            <DxItem data-field="profession" caption="Profissão" />
+            <DxItem data-field="nationality" caption="Nacionalidade" />
+            <DxItem data-field="observations" caption="Observações" />
+            <DxItem data-field="documents" caption="Documentos" />
+          </DxForm>
+        </DxEditing>
         <DxPaging :page-size="10" />
       </DxDataGrid>
       <DxPopup
@@ -115,13 +145,13 @@ function showToast({ message, type }: { message: string; type: string }) {
         <label>Nome</label>
         <input type="text" v-model="formData.name" />
         <label>Telefone</label>
-        <input type="text" v-model="formData.phone"/>
+        <input type="text" v-model="formData.phone" />
         <label>Email</label>
         <input type="text" v-model="formData.email" />
         <label>Endereço</label>
-        <input type="text" v-model="formData.address"/>
+        <input type="text" v-model="formData.address" />
         <label>CPF/CNPJ</label>
-        <input type="text" v-model="formData.cpfCnpj"/>
+        <input type="text" v-model="formData.cpfCnpj" />
         <label>Data de nascimento</label>
         <input type="date" v-model="formData.dateOfBirth" />
         <label>Genero</label>
@@ -137,6 +167,20 @@ function showToast({ message, type }: { message: string; type: string }) {
         <div class="btn-popup">
           <DxButton text="Salvar" @click="handleSaveCustomerOnClick" />
           <DxButton text="Fechar" @click="isModalOpen = false" />
+        </div>
+      </DxPopup>
+      <DxPopup :visible="isModalRowOpen" title="Detalhes do cliente" :width="500">
+        <p>Cliente: <b>{{ selectedDocument?.name }}</b></p>
+        <p>Telefone: <b>{{ selectedDocument?.phone }}</b></p>
+        <p>Endereço: <b>{{ selectedDocument?.address }}</b></p>
+        <p>Descrição: <b>{{ selectedDocument?.observations }}</b></p>
+        <p>CPJ/CNPJ: <b>{{ selectedDocument?.cpfCnpj }}</b></p>
+        <p>Genero: <b>{{ selectedDocument?.gender }}</b></p>
+        <p>Estado cívil: <b>{{ selectedDocument?.maritalStatus }}</b></p>
+        <p>Profissão: <b>{{ selectedDocument?.profession }}</b></p>
+        <p>Nacionalidade: <b>{{ selectedDocument?.nationality }}</b></p>
+        <div class="close-modal-btn">
+          <DxButton text="Fechar modal" @click="isModalRowOpen = false" />
         </div>
       </DxPopup>
     </div>
